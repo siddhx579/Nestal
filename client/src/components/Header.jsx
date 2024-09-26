3d4140594d52b7f61018bf8368ea5570af997160
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2024-09-26 23:41:27 +0530

    complete update user functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
-import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export default function Header() {
@@ -31,11 +31,10 @@ export default function Header() {
                             <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="profile" />
                         ) : (
                             <li className='text-slate-700 hover:underline'>Sign In</li>
-
                         )}
                     </Link>
                 </ul>
             </div>
         </header>
-    )
+    );
 }
