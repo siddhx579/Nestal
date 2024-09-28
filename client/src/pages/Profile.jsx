aa4bf720640196dcda26264b9ddf09610a2e24c3
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2024-09-28 23:47:12 +0530

    Update Profile.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -103,7 +103,6 @@ export default function Profile() {
             dispatch(signOutUserFailure(error.message));
         }
     }
-
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
