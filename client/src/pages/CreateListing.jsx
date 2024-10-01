c2f4bfdf14c038775bd35c7063ca5e6ba6a1b25e
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2024-10-01 13:42:44 +0530

    Update CreateListing.jsx

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -2,9 +2,11 @@ import { useState } from "react"
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreateListing() {
     const {currentUser} = useSelector(state => state.user);
+    const navigate = useNavigate();
     const [files, setFiles] = useState([]);
     const [formData, setFormData] = useState({
         imageUrls: [],
@@ -15,7 +17,7 @@ export default function CreateListing() {
         bedrooms: 1,
         bathrooms: 1,
         regularPrice: 50,
-        discountPrice: 50,
+        discountPrice: 0,
         offer: false,
         parking: false,
         furnished: false,
@@ -40,8 +42,9 @@ export default function CreateListing() {
                 setFormData({ ...formData, imageUrls: formData.imageUrls.concat(urls), });
                 setImageUploadError(false);
                 setUploading(false);
+                
             }).catch((err) => {
-                setImageUploadError('Image upload failed (2 mb max per image');
+                setImageUploadError('Image upload failed (2 mb max per image)');
                 setUploading(false);
             });
         }else{
@@ -126,6 +129,7 @@ export default function CreateListing() {
             if(data.success === false){
                 setError(data.message);
             }
+            navigate(`/listing/${data._id}`);
             
         } catch (error) {
             setError(error.message);
@@ -207,7 +211,7 @@ export default function CreateListing() {
                             </div>
                         ))
                     }
-                <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Creating...' : 'Create listing'}</button>
+                <button disabled={loading || uploading} className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Creating...' : 'Create listing'}</button>
                 {error && <p className="text-red-700 text-sm">{error}</p>}
                 </div>
             </form>
