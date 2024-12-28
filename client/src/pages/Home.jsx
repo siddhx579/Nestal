947f0bac7273741014173376c36abdcd2ce91e32
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2024-12-28 22:58:37 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import SwiperCore from 'swiper';
 import 'swiper/css/bundle';
+import ListingItem from '../components/ListingItem';
 
 export default function Home() {
     const [offerListings, setOfferListings] = useState([]);
@@ -36,7 +37,7 @@ export default function Home() {
 
         const fetchSaleListings = async () => {
             try {
-                const res = await fetch('api/listinng/get?type=sale&limit=4');
+                const res = await fetch('api/listing/get?type=sale&limit=4');
                 const data = await res.json();
                 setSaleListings(data);
             } catch (error) {
@@ -55,12 +56,12 @@ export default function Home() {
                     <br />
                     place with ease
                 </h1>
-                <div className="text-gray-400 text-xs sm:text-sm">
+                <div className="text-gray-400 text-xl sm:text-sm">
                     Nestal is the best place to find your next perfect place to live
                     <br />
                     We have a wide range of properties for you to choose from you
                 </div>
-                <Link to={"/search"} className="text-xs sm:text-blue-800 font-bold hover:underline">
+                <Link to={"/search"} className="text-xl sm:text-blue-800 font-bold hover:underline">
                     Let's get started...
                 </Link>
             </div>
@@ -73,6 +74,49 @@ export default function Home() {
                     ))
                 }
             </Swiper>
+            <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+                {offerListings && offerListings.length > 0 && (
+                    <div className=''>
+                        <div className='my-3'>
+                            <h2 className='text-2xl font-semibold text-slate-600'>Recent offers</h2>
+                            <Link className='text-sm text-blue-800 hover:underline' to={'/search?offer=true'}>Show more offers</Link>
+                        </div>
+                        <div className='flex flex-wrap gap-4'>
+                            {offerListings.map((listing) => (
+                                <ListingItem listing={listing} key={listing._id} />
+                            ))}
+                        </div>
+                    </div>
+                )}
+
+                {rentListings && rentListings.length > 0 && (
+                    <div className=''>
+                        <div className='my-3'>
+                            <h2 className='text-2xl font-semibold text-slate-600'>Recent places for rent</h2>
+                            <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=rent'}>Show more places for rent</Link>
+                        </div>
+                        <div className='flex flex-wrap gap-4'>
+                            {rentListings.map((listing) => (
+                                <ListingItem listing={listing} key={listing._id} />
+                            ))}
+                        </div>
+                    </div>
+                )}
+                
+                {saleListings && saleListings.length > 0 && (
+                    <div className=''>
+                        <div className='my-3'>
+                            <h2 className='text-2xl font-semibold text-slate-600'>Recent places for sale</h2>
+                            <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=sale'}>Show more places for sale</Link>
+                        </div>
+                        <div className='flex flex-wrap gap-4'>
+                            {saleListings.map((listing) => (
+                                <ListingItem listing={listing} key={listing._id} />
+                            ))}
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     )
-}
+};
