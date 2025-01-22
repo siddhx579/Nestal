2de21161d2267b6e26c4496d65cafbf61985b5a5
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-01-22 18:56:52 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,14 +25,14 @@ export default function Header() {
     }, [location.search]);
 
     return (
-        <header className='bg-slate-200 shadow-md'>
+        <header className='bg-[#B1C9EF] shadow-md'>
             <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
                 <Link to='/'>
-                    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                        <span className='text-slate-500'>Nestal</span>
+                    <h1 className='font-bold sm:text-xl flex flex-wrap '>
+                        <span className='text-[#395886] text-3xl'>Nestal</span>
                     </h1>
                 </Link>
-                <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                <form onSubmit={handleSubmit} className='bg-[#F0F3FA] p-3 rounded-lg flex items-center'>
                     <input type="text"
                         placeholder="Search..."
                         className='bg-transparent focus:outline-none w-24 sm:w-64'
@@ -45,7 +45,7 @@ export default function Header() {
                 </form>
                 <ul className='flex gap-4'>
                     <Link to='/'>
-                        <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+                        <li className='hidden sm:inline text-slate-800 hover:underline'>Home</li>
                     </Link>
                     <Link to='/about'>
                         <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
