3f8a9b5396f8110a3623aefe19425291825468ec
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-20 23:58:41 +0530

    Update listing.controller.js

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -16,7 +16,7 @@ export const deleteListing = async (req, res, next) => {
         return next(errorHandler(404, 'Listing not found!'));
     }
 
-    if (req.user.id !== listing.userRef){
+    if (req.user.id !== listing.userRef) {
         return next(errorHandler(401, 'You can only delete your own listing!'));
     }
 
@@ -30,11 +30,11 @@ export const deleteListing = async (req, res, next) => {
 
 export const updateListing = async (req, res, next) => {
     const listing = await Listing.findById(req.params.id);
-    if (!listing){
+    if (!listing) {
         return next(errorHandler(404, 'Listing not found!'));
     }
 
-    if (req.user.id !== listing.userRef){
+    if (req.user.id !== listing.userRef) {
         return next(errorHandler(401, 'You can only update your listings!'));
     }
 
