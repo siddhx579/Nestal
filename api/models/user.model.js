d1d0fe8d287e2b4cd88bc344beefe1a50f1f823c
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-04-20 23:59:19 +0530

    Update user.model.js

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,9 +17,9 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     },
-}, {timestamps: true}
+}, { timestamps: true }
 );
 
 const User = mongoose.model('User', userSchema);
